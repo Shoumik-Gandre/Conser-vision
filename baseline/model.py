497d0034066dd21f2c2de36a73ff56d5222bcb9d
Shoumik-Gandre <shoumikgandre@gmail.com>
2023-06-04 03:50:01 -0700

    using resnet152

--- baseline/model.py
+++ baseline/model.py
@@ -3,12 +3,11 @@ from torchvision import models
 
 
 def get_baseline_model() -> torch.nn.Module:
-    model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+    model = models.resnet152(weights=models.ResNet152_Weights.DEFAULT)
     model.fc = torch.nn.Sequential(  # type: ignore
         torch.nn.Linear(2048, 100),  # dense layer takes a 2048-dim input and outputs 100-dim
-        torch.nn.BatchNorm1d(100),
         torch.nn.ReLU(inplace=True),  # ReLU activation introduces non-linearity
-        torch.nn.Dropout(0.1),  # common technique to mitigate overfitting
+        torch.nn.Dropout(0.3),  # common technique to mitigate overfitting
         torch.nn.Linear(100, 8),  # final dense layer outputs 8-dim corresponding to our target classes
     )
     return model
