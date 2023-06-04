ee9ec61e4271507d978c4c359c6b24021645681a
Shoumik-Gandre <shoumikgandre@gmail.com>
2023-06-04 01:32:51 -0700

    Added google colab link

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Conser-vision
 These are my solutions for the conser-vision challenge on drivendata.org
 
+## Running this code
+The code requires heavy compute. Use the following colab notebook to run this code on google colab GPUs:
+[Colab](https://colab.research.google.com/drive/1VcAuUlEk6MDA3YZ-XQEa12Iy0n7P0DjL?usp=sharing)
+
 ## Baseline
 The baseline solution uses Resnet50 with Adam Optimizer, 
 cross entropy loss and regularization with weight decay.
--- baseline/model.py
+++ baseline/model.py
@@ -6,7 +6,7 @@ def get_baseline_model() -> torch.nn.Module:
     model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
     model.fc = torch.nn.Sequential(  # type: ignore
         torch.nn.Linear(2048, 100),  # dense layer takes a 2048-dim input and outputs 100-dim
-        # torch.nn.BatchNorm1d(100),
+        torch.nn.BatchNorm1d(100),
         torch.nn.ReLU(inplace=True),  # ReLU activation introduces non-linearity
         torch.nn.Dropout(0.1),  # common technique to mitigate overfitting
         torch.nn.Linear(100, 8),  # final dense layer outputs 8-dim corresponding to our target classes
