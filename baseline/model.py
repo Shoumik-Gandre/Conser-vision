95e4c2709ff14277229159185032e68290e057e3
Shoumik-Gandre <shoumikgandre@gmail.com>
2023-06-04 16:21:18 -0700

    baseline model classifier changed

--- baseline/model.py
+++ baseline/model.py
@@ -5,10 +5,19 @@ from torchvision import models
 def get_baseline_model() -> torch.nn.Module:
     model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
     model.fc = torch.nn.Sequential(  # type: ignore
-        torch.nn.Linear(2048, 100),  # dense layer takes a 2048-dim input and outputs 100-dim
+        torch.nn.Linear(2048, 1024),  # dense layer takes a 2048-dim input and outputs 100-dim
         torch.nn.ReLU(inplace=True),  # ReLU activation introduces non-linearity
-        torch.nn.Dropout(0.3),  # common technique to mitigate overfitting
-        torch.nn.Linear(100, 8),  # final dense layer outputs 8-dim corresponding to our target classes
+        torch.nn.Dropout(0.33),  # common technique to mitigate overfitting
+        torch.nn.Linear(1024, 512),  # dense layer takes a 2048-dim input and outputs 100-dim
+        torch.nn.ReLU(inplace=True),  # ReLU activation introduces non-linearity
+        torch.nn.Dropout(0.33),  # common technique to mitigate overfitting
+        torch.nn.Linear(512, 256),  # dense layer takes a 2048-dim input and outputs 100-dim
+        torch.nn.ReLU(inplace=True),  # ReLU activation introduces non-linearity
+        torch.nn.Dropout(0.33),  # common technique to mitigate overfitting
+        torch.nn.Linear(256, 128),  # dense layer takes a 2048-dim input and outputs 100-dim
+        torch.nn.ReLU(inplace=True),  # ReLU activation introduces non-linearity
+        torch.nn.Dropout(0.33),  # common technique to mitigate overfitting
+        torch.nn.Linear(128, 8),  # final dense layer outputs 8-dim corresponding to our target classes
     )
     return model
 
